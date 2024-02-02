a90da0e2bfcc6e438e617f67a8738b1544ad345c
Pedro Requiao <alonsofts@gmail.com>
2024-02-02 04:10:16 -0500

    add: customs declaration use cases and test

--- src/domain/customer/entities/package-product-info-list.ts
+++
@@ -1,8 +0,0 @@
-import { WatchedList } from '@/core/entities/watched-list'
-import { PackageProductInfo } from './package-product-info'
-
-export class PackageProductInfoList extends WatchedList<PackageProductInfo> {
-    compareItems(a: PackageProductInfo, b: PackageProductInfo): boolean {
-        return a.id.equals(b.id)
-    }
-}
